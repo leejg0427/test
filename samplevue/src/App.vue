829abf83275276975199067de01f1bcf59ce59e0
leejg0427 <71866440+leejg0427@users.noreply.github.com>
2023-04-26 14:35:53 +0900

    1
    1

---
+++ samplevue/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <PageHeader/>
+  <router-view/> <!--페이지 이동이 표시될 영역을 의미함-->
+  <PageFooter/>
+</template>
+
+<script>
+import PageHeader from './components/common/PageHeader.vue'
+import PageFooter from './components/common/PageFooter.vue'
+
+export default {
+  name: 'App',
+  components: {
+    PageHeader,
+    PageFooter
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ samplevue/src/views/home/PageAbout.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="about">
+    <h1>이 페이지는 사이트 설명 페이지 입니다.</h1>
+  </div>
+</template>
+
+<style>
+about{
+    flood-color: deeppink;
+}
+</style>
